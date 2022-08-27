9de130636dde87e6468e42af013c183eb8ef36a7
Alex Cheung <alexc.sfu@gmail.com>
2022-08-26 18:46:39 -0700

    initial setup done

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+const CanvasContainer = styled.div``;
+
+const Canvas: FC = () => {
+  return <CanvasContainer>Canvas</CanvasContainer>;
+};
+
+export default Canvas;
