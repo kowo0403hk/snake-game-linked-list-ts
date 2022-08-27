dd3049a7f2df39f8a429f73709e612d95b445f90
Alex Cheung <alexc.sfu@gmail.com>
2022-08-26 19:00:58 -0700

    added procedures and helper function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { FC } from "react";
 import styled from "styled-components";
+import Canvas from "./components/Canvas";
 
-const Container = styled.div``;
+const Container = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 const App: FC = () => {
-  return <Container>App</Container>;
+  return (
+    <Container>
+      <Canvas />
+    </Container>
+  );
 };
 
 export default App;
