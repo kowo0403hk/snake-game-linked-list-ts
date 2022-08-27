046a24eae91f36d19d291edfe7052d729139eff8
Alex Cheung <alexc.sfu@gmail.com>
2022-08-26 19:14:45 -0700

    added a 10x10 2D array and mapped it as the canvas for the game

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,10 +1,43 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
 
-const CanvasContainer = styled.div``;
+const CanvasContainer = styled.div`
+  outline: 2px solid rgb(134, 154, 189);
+`;
+
+const Row = styled.div`
+  height: 50px;
+`;
+
+const Cell = styled.div`
+  width: 50px;
+  height: 50px;
+  outline: 1px solid rgb(134, 154, 189);
+  display: inline-block;
+`;
+
+const CANVAS_SIZE = 10;
 
 const Canvas: FC = () => {
-  return <CanvasContainer>Canvas</CanvasContainer>;
+  // creating a a 10x10 2D array
+  const matrix = new Array(CANVAS_SIZE)
+    .fill(0)
+    .map((row) => new Array(CANVAS_SIZE).fill(0));
+
+  const [canvas, setCanvas] = useState(matrix);
+
+  // create the rows and cells for the canvas
+  const mappedCanvas = canvas.map((row, rowIndex) => {
+    return (
+      <Row key={rowIndex}>
+        {row.map((cell, cellIndex) => {
+          return <Cell key={cellIndex}></Cell>;
+        })}
+      </Row>
+    );
+  });
+
+  return <CanvasContainer>{mappedCanvas}</CanvasContainer>;
 };
 
 export default Canvas;
